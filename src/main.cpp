265feac089886ec59488f79550f81baf69e3fa8a
Kevin Dohmen <kevindohmen06@gmail.com>
2025-04-11 17:10:26 +0200

    read from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include <cstddef> // for size_t
 
 #include "sha256.h"
@@ -11,14 +12,26 @@ int main(){
     Timer sha256Timer;
     Timer base64Timer;
 
-    std::string input;
-    std::cout << "Enter input: ";
-    std::getline(std::cin, input);
-    std::vector<uint8_t> data(input.begin(), input.end());
+    std::string filePath = "./tmp/inpt.txt";
+
+    std::ifstream inFile;
+    inFile.open(filePath, std::ios::binary);
+
+    inFile.seekg(0, std::ios::end);
+    std::streamsize fileSize = inFile.tellg();
+    inFile.seekg(0, std::ios::beg);
+
+    uint8_t* data = new uint8_t[fileSize];
+    if (!inFile.read(reinterpret_cast<char*>(data), fileSize)) {
+        std::cerr << "Error reading file. (" << filePath << ")" << std::endl;
+        return 1;
+    }
+
+    inFile.close();
 
     size_t* hashLen = new size_t(0);
     sha256Timer.start();
-    uint8_t* hash = SHA256::hash(data.data(), data.size(), hashLen);
+    uint8_t* hash = SHA256::hash(data, fileSize, hashLen);
     sha256Timer.stop();
 
     size_t* b64len = new size_t(0);
@@ -27,7 +40,7 @@ int main(){
     base64Timer.stop();
 
     std::cout << std::endl;
-    std::cout << "Base64 Encoded: " << encoded << std::endl;
+    std::cout << "SHA256 hash (Base64): " << encoded << std::endl;
 
     std::cout << "\nSHA256:\n";
     sha256Timer.printElapsedTimeAndCycles();
